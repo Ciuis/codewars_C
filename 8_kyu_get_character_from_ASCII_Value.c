971189d6c87fd19c9fb454cf57330321e461d32c
ciuis <denis.v.krasnikov@gmail.com>
2023-06-25 23:14:36 +0300

    another solutions done

---
+++ 8_kyu_get_character_from_ASCII_Value.c
@@ -0,0 +1,17 @@
+/*Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+In C, getchar() is an important built-in function, so the function will be named char_of_code to avoid confusion.
+
+Example:
+
+get_char(65)
+
+should return:
+
+'A'
+
+For ASCII table, you can refer to http://www.asciitable.com/*/
+
+char char_of_code (unsigned charcode) {
+    return (char)charcode;
+}
